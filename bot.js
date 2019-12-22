bf5f23786f8e65a6ab5b0086dd62d61b2acc0f82
Jo Colina <jo.colina@weezevent.com>
2019-12-22 12:38:01 +0100

    Refacto: commands + Heroku listener

--- bot.js
+++ bot.js
@@ -1,16 +1,19 @@
 const Sentry = require('./lib/sentry');
+const Heroku = require('./lib/heroku');
 
+// Launch commands
 const Commands = require('./commands/commands');
+Commands.register();
+
 const bot = require('./lib/bot');
 
-// Handle MMR for rocket league, check channel and user
-bot.command('/mmr', (...args) => { Commands.mmr(...args) });
-
+// Help handler
 bot.on('message', ({ content, channel, channel_id }) => {
 	if(content.indexOf('help') > -1) {
 		if(!channel || !channel.is_dm) {
 			return;
 		}
+
 		return bot.message({
 			channel: channel ? channel.id : channel_id,
 			content:
@@ -25,12 +28,28 @@ You can find your tracker id by going to Tracker Network (https://rocketleague.t
 	}
 });
 
+// Start the bot
 bot.connect();
 
+// Set error handlers to send errors to sentry and prevent crashes
+bot.on('error', (e) => {
+	// TODO: find a way to pass params to configure sentry scope
+	Sentry.captureException(e);
+});
+
+// PM should be able to restart process without a problem
 process.on('uncaughtException', (e) => {
 	Sentry.captureException(e);
+	process.exit(1);
 });
 
 process.on('unhandledRejection', (e) => {
 	Sentry.captureException(e);
+	process.exit(1);
+});
+
+// Heroku listener
+const port = process.env.PORT || 1234;
+Heroku.listen({ port }, () => {
+	console.log(`Heroku server listening on port ${port}`);
 });
---
+++ commands/mmr.js
@@ -0,0 +1,51 @@
+const RocketLeague = require('../rocket-league');
+const bot = require('../lib/bot');
+
+module.exports = ({ args: { variables, options } , data }) => {
+	if(!data.channel || !data.channel.is_dm) {
+		return bot.message({
+			channel: data.channel_id,
+			content: 'Please request MMR only in the #rocketleague channel ;)'
+		});
+	}
+
+	let [ user_arg_id ] = variables;
+
+	// Fetch MMR and send
+	let user_id = parseInt(user_arg_id) || 972527;
+	RocketLeague.mmr([user_id]).then(players => {
+		let concerned_player = players.find(p => p.PlayerId === user_id);
+		if(!concerned_player) {
+			return bot.message({
+				channel: data.channel_id,
+				content: `Oopsy, could not find any player with id ${user_id}`
+			});
+		}
+		// Create messages and send
+		let fields = [];
+		for(let mmr of concerned_player.mmr) {
+			fields.push({
+				name: `${mmr.Value.Label} (_**${mmr.Other.subtitle}**_ - *${mmr.Other.subtitle2}*)`,
+				value:`**${mmr.Value.ValueInt}**`
+			});
+		}
+
+		return bot.message({
+			channel: data.channel_id,
+			content: {
+				embed: {
+					fields,
+					thumbnail: {
+						url: "https://img.favpng.com/12/11/8/rocket-league-logo-t-shirt-png-favpng-9tHvKE75AdYSJurjXbQvnKZRd.jpg"
+					},
+					author: {
+						name: concerned_player.platformUserHandle
+					},
+					description: `[Rank distribution](https://rocketleague.tracker.network/distribution/11)`
+				}
+			}
+		});
+	}).then(() => {}).catch(e => {
+		console.error(e);
+	});
+};
