b1f823d1047a4dca347a68b4690f1da74b6fba4f
Jo Colina <jo.colina@weezevent.com>
2019-12-22 12:46:33 +0100

    Commands: actually register commands

--- commands/commands.js
+++ commands/commands.js
@@ -3,14 +3,12 @@ const bot = require('../lib/bot');
 // Commands
 const mmr = require('./mmr');
 
-let commands = {
-	mmr
-}
+let commands = { mmr };
 
 // Exported commands
 module.exports = {
 	register: () => {
-		for(let [name, command] in Object.entries(commands)) {
+		for(let [name, command] of Object.entries(commands)) {
 			bot.command(`/${name}`, (...args) => command(...args));
 		}
 	}
